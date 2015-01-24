74f283d325e3f65da4a38ad3ce111f06ef72a244
Toni Ristola <tristola@gmail.com>
2015-01-24 08:50:00 +0200

    updated styles

--- gulpfile.js
+++ gulpfile.js
@@ -6,43 +6,42 @@ var ngAutoBootstrap = require('gulp-ng-autobootstrap');
 var browserSync = require('browser-sync');
 
 
-gulp.task('default',['task-module','user-module','views','browserify','browser-sync']);
+gulp.task('default', ['task-module', 'user-module', 'views', 'browserify', 'browser-sync']);
 
 gulp.task('task-module', function() {
-    return gulp
-        .src('app/test/**/*.js')
-        .pipe(ngAutoBootstrap(ngconfig))
-        .pipe(gulp.dest('app/test'));
+  return gulp
+    .src('app/test/**/*.js')
+    .pipe(ngAutoBootstrap(ngconfig))
+    .pipe(gulp.dest('app/test'));
 });
 gulp.task('user-module', function() {
-    return gulp
-        .src('app/user/**/*.js')
-        .pipe(ngAutoBootstrap(ngconfig))
-        .pipe(gulp.dest('app/user'));
+  return gulp
+    .src('app/user/**/*.js')
+    .pipe(ngAutoBootstrap(ngconfig))
+    .pipe(gulp.dest('app/user'));
 });
 
 gulp.task('browserify', function() {
-gulp.src(['app/main.js'])
-.pipe(browserify({
-insertGlobals: true,
-debug: false
-}))
-.pipe(concat('bundle.js'))
-.pipe(gulp.dest('dist/js'));
+  gulp.src(['app/main.js'])
+    .pipe(browserify({
+      insertGlobals: true,
+      debug: false
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 
 gulp.task('views', function() {
   gulp.src('app/index.html')
-  .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'));
 });
-
 
 // Static server
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "dist/"
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: "dist/"
+    }
+  });
 })
